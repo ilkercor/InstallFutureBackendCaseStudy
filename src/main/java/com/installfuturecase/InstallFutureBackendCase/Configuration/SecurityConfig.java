0dbe7df7337bbe241e8ad8f887fcacc3f02990b2
ilkercor <ilker.cor19@gmail.com>
2023-07-13 03:07:48 +0300

    Added authentication from database using JDBC for Spring Security.

--- src/main/java/com/installfuturecase/InstallFutureBackendCase/Configuration/SecurityConfig.java
+++ src/main/java/com/installfuturecase/InstallFutureBackendCase/Configuration/SecurityConfig.java
@@ -2,40 +2,64 @@ package com.installfuturecase.InstallFutureBackendCase.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public UserDetailsManager  userDetailsManager (DataSource dataSource){
+        JdbcUserDetailsManager theUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        theUserDetailsManager
+                .setUsersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?");
+
+
+        theUserDetailsManager
+                .setAuthoritiesByUsernameQuery("SELECT username, role FROM users WHERE username = ?");
+        return theUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET,"/user/list").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/user/add").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/user/update").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/user/delete").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/user/**").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf->csrf.disable());
+        return http.build();
+    }
+
+/*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
         UserDetails admin = User.builder()
                 .username("admin")
                 .password("{noop}admin")
-                .roles("admin")
+                .roles("Admin")
                 .build();
 
         UserDetails ilker = User.builder()
                 .username("ilker")
                 .password("{noop}ilker")
-                .roles("user")
+                .roles("User")
                 .build();
 
         return new InMemoryUserDetailsManager(admin,ilker);
     }
-
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http
-                .csrf().disable();
-        return http.build();
-    }
-
-
+*/
 }
